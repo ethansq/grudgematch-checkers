a357d5a9d6ba36fab6c34afb9650d963c8e180c2
Ethan Han <ethan.han47@gmail.com>
2017-12-28 23:46:57 -0500

    implemented 'active' pieces, where pieces can only make kill-moves after it's made
    ; TODO, still, active pieces are only activated after a kill, so make sure this doesn't happen on a regular move

--- src/checkers.js
+++ src/checkers.js
@@ -42,11 +42,12 @@ class Board extends Component {
     constructor(props) {
         super(props);
 
+        // default board with starting positions
         var cells = [];
         // var b = [8, 1, 17, 10, 3, 19, 12, 5, 21, 14, 7, 23];
         // var r = [40, 56, 49, 42, 58, 51, 44, 60, 53, 46, 62, 55];
-        var b = [26, 28]; // @TEST
-        var r = [35]; // @TEST
+        var b = [26, 28, 12, 14]; // @TEST
+        var r = [35, 37]; // @TEST
 
         for (var i=0; i<64; i++) {
             if (b.indexOf(i) !== -1) {
@@ -61,9 +62,11 @@ class Board extends Component {
         this.state = {
             cells: cells,
             selected: null, // index of selected piece
-            auxiliary: [] // options based on the selected piece
+            auxiliary: [], // options based on the selected piece
+            active: null
         };
 
+        // bindings
         this.renderCell = this.renderCell.bind(this);
         this.renderRow = this.renderRow.bind(this);
         this.handleCellClick = this.handleCellClick.bind(this);
@@ -120,7 +123,27 @@ class Board extends Component {
         // check which auxiliary locations we need to eliminate (no pieces to jump,
         // space is already occupied, etc)
         var _auxiliary = auxiliary.map((ele, index) => {
-            if (false
+            if (this.state.active !== null) {
+                // must be within bounds
+                if (ele < 0 || ele > 63) {
+
+                }
+                // while active, inner moves are not allowed
+                else if (index % 2 === 0) {
+                    return -1;
+                } else {
+                    if (this.state.cells[auxiliary[index-1]] === null || // if no piece to kill, cannot move to this dest
+                            this.state.cells[auxiliary[index-1]].colour === colour) { // cannot kill friendlies
+                        return -1;
+                    }
+                    // unless this piece is a king, can only kill forwards
+                    else if (((colour === 'r' && ele > i) && !this.state.cells[i].king)
+                        || ((colour === 'b' && ele < i) && !this.state.cells[i].king)) {
+                        return -1;
+                    }
+                    return ele;
+                }
+            } else if (false
                 // our rules chart for determining whether a cell is a valid destination
                 // or not
                 || (ele < 0 || ele > 63) // space needs to be on the board
@@ -157,38 +180,58 @@ class Board extends Component {
         const inner = [-9, -7, 7, 9];
         const outer = [-18, -14, 14, 18];
 
-        if (outer.indexOf(_dest - _from) !== -1) { // if dest is an outer cell, remove the killed piece
+        // if dest is an outer cell, remove the killed piece
+        if (outer.indexOf(_dest - _from) !== -1) {
             this.state.cells[_from + inner[outer.indexOf(_dest - _from)]] = null;
         }
 
+        // copy the board
         var cells = this.state.cells.slice();
+        // if the piece has reached the end of the board, promote to king
+        var promote =
+            (this.state.cells[_from].colour === 'r' && _dest < 8) ||
+                (this.state.cells[_from].colour === 'b' && _dest > 55);
+
         cells[_dest] = cells[_from]; // move piece
         cells[_from] = null; // delete piece from old position
+        cells[_dest].king = promote;
 
         this.setState(
-            {cells: cells}, // update cells
+            {
+                // update cells
+                cells: cells,
+                // piece is now active. Only this piece can move, and any subsequent
+                // moves must be kills
+                active: _dest
+            },
             () => { // once state is updated, also update the current selection
                 this.handleSelectCell(_dest);
             }
         );
-
     }
 
     handleCellClick(i) {
+        // if we're clicking to move a piece, perform the move
+        if (this.state.auxiliary.indexOf(i) !== -1) {
+            this.handleMove(i);
+        }
+        // if we have an active piece, then we can only show subsequent kill-moves
+        // for that active piece
+        // do not allow de-selection
+        else if (this.state.active !== null) {
+            return;
+        }
         // if we're clicking a friendly piece, "select" that piece
         // using 'r' default for now
-        if (this.state.cells[i] !== null && this.state.cells[i].colour === 'r') {
+        else if (this.state.cells[i] !== null && this.state.cells[i].colour === 'r') {
             this.handleSelectCell(i);
-        }
-        // if we're clicking to move a piece, perform the move
-        else if (this.state.auxiliary.indexOf(i) !== -1) {
-            this.handleMove(i);
         }
         // if we're clicking an empty space, de-select
         else {
             this.setState({
                 selected: null,
-                auxiliary: []
+                auxiliary: [],
+                active: null
             })
         }
     }
